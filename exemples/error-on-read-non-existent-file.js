d90f2188a3b594089e61d5afed0c9f03dc98f50d
Carlos Alberto <carloshtcursos@gmail.com>
2021-10-06 00:14:12 -0400

    feat: add exemples

---
+++ exemples/error-on-read-non-existent-file.js
@@ -0,0 +1,28 @@
+const { resolveLeft, resolveRight } = require("../lib/either");
+const fs = require("fs/promises");
+
+const promiseResolver = (promise) =>
+  promise.then(resolveRight).catch(resolveLeft)
+
+class CustomError {
+  constructor(error) {
+    this.name = error.name;
+    this.stack = error.stack;
+    this.message = error.message;
+  }
+}
+
+const readNonExistentFile = async () => {
+  const either = await promiseResolver(fs.readFile("non-existent.txt"));
+  if (either.isLeft()) {
+    return resolveRight(new CustomError(either.value));
+  }
+
+  return resolveRight(either.value);
+};
+
+(async () => {
+  const res = await readNonExistentFile();
+
+  console.log(res.value);
+})();
